22245ac9556af26ef011156609540c853b71ff52
JustinHo09 <hojustin09@gmail.com>
2026-04-05 22:22:48 -0500

    I added a fly animation, a weight mechanic, gravity mechanic, and some UI elements.
    I added a fly animation to play after the jump one finishes. I added a weight feature where the player can adjust the weight of the dragon, which will affect its jump, which is done using Unity physics engine using addForce. Then I added some UI elements like a GameOver screen and a score tracker, and a slider to indicate the player's weight.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,35 +1,105 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public float hp;
+    public float upAmount;
+    public float maxWeight;
+
+    public GameObject gameOver;
+    public Slider weight;
+    public TMP_Text score;
+    public int points;
 
     public bool jumping;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        weight.maxValue = maxWeight;
+        weight.minValue = 1.0f;
+        weight.value = GetComponent<Rigidbody2D>().mass;
         jumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.leftArrowKey.wasPressedThisFrame || Keyboard.aKey.wasPressedThisFrame)
+        Vector2 newPos = new Vector2(transform.position.x, transform.position.y + 1.0f);
+        weight.transform.position = newPos;
+        if (hp <= 0)
         {
-            GetComponent<RigidBody2d>().mass
+            GetComponent<Animator>().SetTrigger("dies");
+            gameOver.SetActive(true);
+            this.enabled = false;
         }
         
-        if (Keyboard.rightArrowKey.wasPressedThisFrame || Keyboard.dKey.wasPressedThisFrame)
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.aKey.wasPressedThisFrame)
         {
-            
+            if (GetComponent<Rigidbody2D>().mass > 1.0f) {
+                GetComponent<Rigidbody2D>().mass -= 1.0f;
+            }else{
+                GetComponent<Rigidbody2D>().mass = 1.0f;
+            }
+            weight.value = GetComponent<Rigidbody2D>().mass;
+        }
+        
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            if (GetComponent<Rigidbody2D>().mass < maxWeight)
+            {
+                GetComponent<Rigidbody2D>().mass += 1.0f;
+            }else
+            {
+                GetComponent<Rigidbody2D>().mass = maxWeight;
+            }
+            weight.value = GetComponent<Rigidbody2D>().mass;
         }
 
-        if (Keyboard.spaceKey.wasPressedThisFrame && !jumping)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !jumping)
         {
-            jumping == true;
+            jumping = true;
+            GetComponent<Animator>().SetTrigger("jump");
+            GetComponent<Rigidbody2D>().AddForceY(upAmount, ForceMode2D.Impulse);
+        }
+
+        if (Keyboard.current.shiftKey.isPressed && jumping)
+        {
+            GetComponent<Rigidbody2D>().gravityScale += 1.0f;
         }
     }
-    
-    public void onCollis
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag.Equals("obstacle")) {
+            GetComponent<Animator>().SetTrigger("hit");
+        }
+    }
+
+    public void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.tag == "Border")
+        {
+            jumping = false;
+            GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+            GetComponent<Animator>().SetTrigger("landed");
+        }
+    }
+
+    public void OnCollusionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Border")
+        {
+            jumping = true;
+        }
+    }
+
+    public void updateScore(int newPoints)
+    {
+        points += newPoints;
+        score.SetText("SCORE: " + points);
+    }
 }
